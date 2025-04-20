32a7905f19ec983f9bec396bc2215050e1cfb3b3
Sam Faull <s.faull@outlook.com>
2025-04-20 18:13:28 +0100

    Better state naming and use of parenthesis and default in switch case

--- components/victron/vedparse.c
+++ components/victron/vedparse.c
@@ -5,12 +5,12 @@
 
 typedef enum
 {
-    IDLE = 0,
-    RECORD_BEGIN,
-    RECORD_NAME,
-    RECORD_VALUE,
-    CHECKSUM,
-    RECORD_HEX,
+    VEDPARSE_STATE_IDLE = 0,
+    VEDPARSE_STATE_RECORD_BEGIN,
+    VEDPARSE_STATE_RECORD_NAME,
+    VEDPARSE_STATE_RECORD_VALUE,
+    VEDPARSE_STATE_CHECKSUM,
+    VEDPARSE_STATE_RECORD_HEX,
 } VEDPARSE_state_e;
 
 typedef struct 
@@ -82,7 +82,7 @@ void VEDPARSE_init(void)
 bool VEDPARSE_process(uint8_t inbyte)
 {
     bool frame_ready = false;
-#if 0
+
     // auto init
     if(!_internal.initialised)
         VEDPARSE_init();
@@ -90,9 +90,9 @@ bool VEDPARSE_process(uint8_t inbyte)
     //printf("State: %d\n", _internal.state);
 
     // check for hex records, we will ignore these
-    if ( (inbyte == ':') && (_internal.state != CHECKSUM) ) 
+    if ( (inbyte == ':') && (_internal.state != VEDPARSE_STATE_CHECKSUM) ) 
     {
-        _internal.state = RECORD_HEX;
+        _internal.state = VEDPARSE_STATE_RECORD_HEX;
     }
 
     // compute the checksum on the fly
@@ -107,55 +107,61 @@ bool VEDPARSE_process(uint8_t inbyte)
     // run the sate machine
     switch(_internal.state) 
     {
-        case IDLE:  /* wait for \n of the start of an record */
-
+        case VEDPARSE_STATE_IDLE:  /* wait for \n of the start of an record */
+        {
             switch(inbyte) 
             {
                 case '\n':
-                    _internal.state = RECORD_BEGIN;
+                    _internal.state = VEDPARSE_STATE_RECORD_BEGIN;
                 break;
                 case '\r': /* Skip */
                 default:
                     // do nothing
                 break;
             }
-        break;
+        } break;
 
-        case RECORD_BEGIN:
-            _internal.prop.key[_internal.prop_char_index++] = inbyte; // first we will be writing to the key
-            _internal.state = RECORD_NAME; 
-        break;
-
-        case RECORD_NAME:
-        switch(inbyte) // The record name is being received, terminated by a \t
+        case VEDPARSE_STATE_RECORD_BEGIN:
         {
-            case '\t':
-                // the Checksum record indicates a EOR
-                if ( _internal.prop_char_index < VEDPARSE_KEY_LENGTH_MAX ) 
-                {
-                    _internal.prop.key[_internal.prop_char_index++] = 0; /* Zero terminate */
-                    if (strcmp(_internal.prop.key, checksumTagName) == 0)  // see if this is the checksum property
-                    {
-                        _internal.state = CHECKSUM;
-                        break;
-                    }
-                }
-                _internal.prop_char_index = 0; // reset the indexer
-                _internal.state = RECORD_VALUE;
-            break;
-            default:
-                // add byte to name, but do no overflow
-                if ( _internal.prop_char_index < VEDPARSE_KEY_LENGTH_MAX  )
-                    _internal.prop.key[_internal.prop_char_index++] = inbyte; 
-            break;
-        }
-        break;
+            _internal.prop.key[_internal.prop_char_index++] = inbyte; // first we will be writing to the key
+            _internal.state = VEDPARSE_STATE_RECORD_NAME; 
+        } break;
 
-        case RECORD_VALUE:
+        case VEDPARSE_STATE_RECORD_NAME:
+        {
+            switch(inbyte) // The record name is being received, terminated by a \t
+            {
+                case '\t':
+                {
+                    // the Checksum record indicates a EOR
+                    if ( _internal.prop_char_index < VEDPARSE_KEY_LENGTH_MAX ) 
+                    {
+                        _internal.prop.key[_internal.prop_char_index++] = 0; /* Zero terminate */
+                        if (strcmp(_internal.prop.key, checksumTagName) == 0)  // see if this is the checksum property
+                        {
+                            _internal.state = VEDPARSE_STATE_CHECKSUM;
+                            break;
+                        }
+                    }
+                    _internal.prop_char_index = 0; // reset the indexer
+                    _internal.state = VEDPARSE_STATE_RECORD_VALUE;
+                } break;
+                default:
+                {
+                    // add byte to name, but do no overflow
+                    if ( _internal.prop_char_index < VEDPARSE_KEY_LENGTH_MAX  )
+                        _internal.prop.key[_internal.prop_char_index++] = inbyte; 
+                } break;
+            }
+        } break;
+
+        case VEDPARSE_STATE_RECORD_VALUE:
+        {
             // The record value is being received. The \r indicates a new record.
             switch(inbyte) 
             {
                 case '\n':
+                {
                     // forward record, only if it could be stored completely
                     if ( _internal.prop_char_index < VEDPARSE_VALUE_LENGTH_MAX ) 
                     {
@@ -163,19 +169,23 @@ bool VEDPARSE_process(uint8_t inbyte)
                         doAddProperty(&_internal.frame, &_internal.prop); // add the completed property to the frame struct
                     }
                     _internal.prop_char_index = 0; // reset the indexer
-                    _internal.state = RECORD_BEGIN;
-                break;
-            case '\r': /* Skip */
-            break;
-            default:
-                // add byte to value, but do no overflow
-                if ( _internal.prop_char_index < VEDPARSE_VALUE_LENGTH_MAX)
-                    _internal.prop.value[_internal.prop_char_index++] = inbyte; 
-            break;
+                    _internal.state = VEDPARSE_STATE_RECORD_BEGIN;
+                } break;
+                case '\r': /* Skip */
+                {
+                    // do nothing
+                } break;
+                default:
+                {
+                    // add byte to value, but do no overflow
+                    if ( _internal.prop_char_index < VEDPARSE_VALUE_LENGTH_MAX)
+                        _internal.prop.value[_internal.prop_char_index++] = inbyte; 
+                } break;
             }
+        }
         break;
 
-        case CHECKSUM:
+        case VEDPARSE_STATE_CHECKSUM:
         {
             //printf("CSUM is %02X\n", _internal.checksum);
             bool valid = (_internal.checksum == 0);
@@ -186,21 +196,27 @@ bool VEDPARSE_process(uint8_t inbyte)
                 printf("[CHECKSUM] Invalid frame\n");
             #endif
             _internal.checksum = 0;
-            _internal.state = IDLE;
+            _internal.state = VEDPARSE_STATE_IDLE;
             frame_ready = true;
             // frameEndEvent(valid); // TODO: add callback
-            break;
-        }
+        } break;
 
-        case RECORD_HEX:
-        if (!doIsHexChar(inbyte)) // TODO: check me
+        case VEDPARSE_STATE_RECORD_HEX:
         {
-            _internal.checksum = 0;
-            _internal.state = IDLE;
-        }
-        break;
+            if (!doIsHexChar(inbyte)) // TODO: check me
+            {
+                _internal.checksum = 0;
+                _internal.state = VEDPARSE_STATE_IDLE;
+            }
+        } break;
+
+        default:
+        {
+            // unknown state
+            _internal.state = VEDPARSE_STATE_IDLE;
+        } break;
     }
-#endif
+
     return frame_ready;
 }
 
@@ -236,5 +252,5 @@ void VEDPARSE_reset(void)
  */
 bool VEDPARSE_frame_started(void)
 {
-    return _internal.state != IDLE;
+    return (_internal.state != VEDPARSE_STATE_IDLE);
 }
