b520c65581198226c19085f6a806c13e5ce08219
Sam Faull <s.faull@outlook.com>
2025-04-20 18:16:36 +0100

    Commented out process

--- components/victron/vedparse.c
+++ components/victron/vedparse.c
@@ -88,7 +88,7 @@ bool VEDPARSE_process(uint8_t inbyte)
         VEDPARSE_init();
     
     //printf("State: %d\n", _internal.state);
-
+#if 0
     // check for hex records, we will ignore these
     if ( (inbyte == ':') && (_internal.state != VEDPARSE_STATE_CHECKSUM) ) 
     {
@@ -216,7 +216,7 @@ bool VEDPARSE_process(uint8_t inbyte)
             _internal.state = VEDPARSE_STATE_IDLE;
         } break;
     }
-
+#endif
     return frame_ready;
 }
 
